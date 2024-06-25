3f82f22a5d3291c312c9c36fbe9492015a84f052
Suzuki-ryutaro1 <sampleow0910@gmail.com>
2024-06-25 22:30:53 +0900

    データを取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        // 追加
+        $todo = new Todo();
+        $todoList = $todo->all();
+        dd($todoList);
+
         return view('todo.index');
     }
 }
